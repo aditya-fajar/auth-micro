4b6c140796bd4df6379fc2ca57cc1f44e6e3d4ed
Khani Launggu <kanilaunggu@gmail.com>
2019-12-17 14:33:33 +0700

    GO: Perbaharui mdl_akses_token.go, mdl_akses_user.go

---
+++ model/mdl_akses_user.go
@@ -0,0 +1,8 @@
+package model
+
+type Token struct {
+	ID       uint64 `json:"id" gorm:"primary_key"`
+	Name     string `json:"name" gorm:"column:name"`
+	Email    string `json:"email" gorm:"column:email"`
+	Password string `json:"password"   gorm:"column:password"`
+}
