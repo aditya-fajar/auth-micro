9f50f71ac6d5bbbb6646b0b35feeb4a40064eba1
Agung Wicaksono <agungw@ecampus.ut.ac.id>
2019-12-17 15:22:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pfpmaniac123/auth-micro/auth"
+	"github.com/pfpmaniac123/auth-micro/config"
+)
 
+func main() {
+	r := gin.Default()
+	db := config.DBInit()
+	login := auth.New(db)
+	//product := product.Product{DB: db}
+
+	//r.GET("/products", product.GetProducts)
+	//r.POST("/products", product.CreateProduct)
+
+	//r.GET("/test", func)
+	r.POST("/login", login.LoginUser)
+
+	r.POST("/register", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Register",
+		})
+	})
+
+	r.PATCH("/update", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Update",
+		})
+	})
+
+	r.Run()
 }
